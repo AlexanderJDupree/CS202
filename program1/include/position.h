ce71f227368e54b60056ac9b5719ad90899546ca
alexanderjdupree <alexander.j.dupree@gmail.com>
2018-09-28 15:47:07 -0700

    Started program 1

---
+++ program1/include/position.h
@@ -0,0 +1,29 @@
+/*
+File: position.h
+
+Description: 
+
+Author: Alexander DuPree
+
+Class: CS202
+
+Assignment: program 1
+
+Date: 09/28/2018
+*/
+
+#ifndef POSITION_H
+#define POSITION_H
+
+class Position
+{
+  public:
+
+  protected:
+
+  private:
+
+};
+
+
+#endif // POSITION_H
---
+++ program1/include/vehicle.h
@@ -0,0 +1,29 @@
+/*
+File: vehicle.h
+
+Description: 
+
+Author: Alexander DuPree
+
+Class: CS202
+
+Assignment: program 1
+
+Date: 09/28/2018
+*/
+
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+class Vehicle
+{
+  public:
+
+  protected:
+
+  private:
+
+};
+
+#endif // VEHICLE_H
+
